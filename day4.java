a16f1f4013350f4c4d0d81b262f14a36240e3a89
msdhoni7781 <91384562+msdhoni7781@users.noreply.github.com>
2021-10-30 11:39:48 +0530

    Create day4.java

---
+++ day4.java
@@ -0,0 +1,12 @@
+package prac;
+
+public class day3 {
+    public static void main(String[] args) {
+      int arr[]={3,5,7,8};
+      int target =3;
+
+      int a=bin(arr,target);
+
+        System.out.println(a);
+
+    }
